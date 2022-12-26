b174021cd205e7d24b3812a34b77ae17b20a25b0
grisha0008 <bogovgriga@gmail.com>
2022-12-26 22:03:21 +0500

    k1

--- src/lr4/R4.java
+++ src/lr4/R4.java
@@ -14,21 +14,21 @@ public class R4 {
             int j = i+1;
             grath[i]=new char[j];
         }
-        int z = 0; //переменная для вывода количества строк
-        for (int i =0; i < r; i++){//заполнение массива
+        int z = 0;
+        for (int i =0; i < r; i++){
             for (int j = 0; j <i+1; j++){
                 grath[i][j] = '@';
             }
         }
-        for (int i =0; i < r; i++){//печать массива
-            int count = i+1;//переменная для повышения номера индекса для вывода на консоль
+        for (int i =0; i < r; i++){
+            int count = i+1;
             System.out.print("номер строки " + count + " : ");
             for (int j = 0; j < grath[i].length; j++){
                 System.out.print(grath[i][j]+" ");
                 z++;
             }
             System.out.println(" Количество символов в строке " + z);
-            z = 0; //обнуление счетчика
+            z = 0;
         }
     }
 }
---
+++ src/lr5/R1.java
@@ -0,0 +1,34 @@
+package lr5;
+
+public class R1 {
+    public static void main(String[] args) {
+        Example Example1 = new Example();
+        Example1.viewCode();
+
+        Example1.Set('A');
+        System.out.println("Char code = " + Example1.getCodeSymbol());
+
+        Example1.viewCode();
+    }
+}
+class Example {
+
+    private char ch;
+
+    public void Set(char ch1) {
+        ch = ch1;
+    }
+
+    public int getCodeSymbol() {
+        return ch;
+    }
+    public void viewCode() {
+        System.out.println("ch1 = " + ch);
+        int code = ch;
+        System.out.println("ch1 = " + code);
+    }
+
+
+}
+
+
